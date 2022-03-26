19929d1b213814af443bd6d880d25447dd23b7da
Frog <75155258+r-40021@users.noreply.github.com>
2022-03-26 19:10:09 +0900

    [Add]Auto zip

---
+++ zip.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dpkg -s zip >/dev/null 2>&1
+if [ ! $? -eq 0 ]; then
+  echo "zip をインストールしてください"
+  echo;
+  echo "sudo apt install zip"
+else
+  zip ../replace-font-firefox.zip -r * -x .git README.md LICENSE
+fi

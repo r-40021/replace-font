8b427745da913dad4a566ab69cac4f295d606012
Frog <frog@frogapp.net>
2022-12-27 11:34:14 +0900

    Replace in frame

--- preload-fonts.js
+++ preload-fonts.js
@@ -1,25 +1,31 @@
 (() => {
+  createPreloadTag(document); // ルートドキュメントにフォント読み込み
+  const iframes = document.getElementsByTagName('iframe');
+  for (iframe of iframes) {
+    createPreloadTag(iframe.contentDocument); // iframe内のドキュメントにフォント読み込み
+  };
+})();
+
+function createPreloadTag(elem) {
+  if (!elem) return;
   const baseClassName = 'preloadFontTag';
   const randomInt = Math.floor(Math.random() * 10000) // クラス名の被りを防止するための乱数
   const className = baseClassName + randomInt;
-
-  function createPreloadTag(path) {
-    const preloadTag = document.createElement('link');
+  const weightList = ['Regular', 'Bold'];
+  weightList.map((weight) => {
+    const preloadTag = elem.createElement('link');
     preloadTag.setAttribute('rel', 'preload');
     preloadTag.setAttribute('as', 'font');
-    preloadTag.setAttribute('href', chrome.runtime.getURL(`fonts/NotoSansCJKjp-${path}-subset.woff2`));
+    preloadTag.setAttribute('href', chrome.runtime.getURL(`fonts/NotoSansCJKjp-${weight}-subset.woff2`));
     preloadTag.setAttribute('crossorigin', true);
     preloadTag.classList = className;
-    document.head.appendChild(preloadTag);
+    elem.head.appendChild(preloadTag);
     preloadTag.addEventListener('load', () => {
-      const loadCSS = document.createElement('link');
+      const loadCSS = elem.createElement('link');
       loadCSS.setAttribute('rel', 'stylesheet');
       loadCSS.setAttribute('type', 'text/css');
-      loadCSS.setAttribute('href', chrome.runtime.getURL(`css/replacefont-extension-${path.toLowerCase()}.css`));
-      document.head.appendChild(loadCSS);
-    })
-  }
-
-  createPreloadTag('Regular');
-  createPreloadTag('Bold');
-})();
+      loadCSS.setAttribute('href', chrome.runtime.getURL(`css/replacefont-extension-${weight.toLowerCase()}.css`));
+      elem.head.appendChild(loadCSS);
+    });
+  });
+}
